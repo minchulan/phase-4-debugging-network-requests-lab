4d681bad94d8c2eca3b4b81351243039a779ac6b
Minchul An <min@minchulan.io>
2023-04-26 18:02:09 -0600

    commit tree 1a1d6de5598758821860ef77a794767977d80a51

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,25 +1,40 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
 
+  # GET all 
   def index
     toys = Toy.all
     render json: toys
   end
 
+  def show
+    toy = Toy.find_by(id: params[:id])
+    render json: toy 
+  end 
+
+  #POST
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
+  #PATCH
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
   end
 
+  #DELETE
   def destroy
-    toy = Toy.find_by(id: params[:id])
-    toy.destroy
-    head :no_content
+    toy = Toy.find_by(id: params[:id]) #find the item
+    if toy #if found
+      toy.destroy #destroy it 
+      head :no_content #send head no content
+    else  #else
+      render json: {error: "toy not found"} #render toy not found
+    end 
+
+
   end
 
   private
